446564a4338dce38257d10f24c478a47cd02eacb
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-05 00:12:48 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import ReactMarkdown from 'react-markdown';
+import React from 'react'
+import MarkdownApp from './MarkdownApp'
 
 const App = () => {
-    const [markdown, setMarkdown]= useState();
-    const [loading, setLoading]= useState(false);
-    const handleChange=(e)=>{
-        setLoading(true);
-        setMarkdown(e.target.value);
-
-    }
-    useEffect(()=>{
-        if(markdown){
-            setLoading(false);
-        }
-    },[markdown])
   return (
     <div>
-        <div className='app'>
-<h1>Markdown Preview Application </h1>
-<div style={{display:"flex"}}>
- <textarea className='textarea'
-    value={markdown}
-    onChange={(e)=>setMarkdown(e.target.value)}
-    placeholder='write your markdown here...'
-    rows="15"
-    cols="50"
-
- >
-
- </textarea>
- <div className='preview' style={{marginLeft:"20px", border:"1px solid black"}}>
-<ReactMarkdown>{markdown}</ReactMarkdown>
- </div>
-</div>
-        </div>
-        {loading && <p className='loadinng'> Loading preview...</p>}
+        <MarkdownApp />
     </div>
   )
 }
