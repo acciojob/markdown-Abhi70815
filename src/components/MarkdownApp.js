9d68bb290dd65e75fd3414c397cf99328c903a79
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-09 18:30:35 +0530

    save

--- src/components/MarkdownApp.js
+++ src/components/MarkdownApp.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MarkdownApp = ({setMarkedText})=> {
  
   return (
-    <textarea rows={24} cols={50} onChange={(e)=>setMarkedText(e.target.value)} className='textarea'>Hello world</textarea>
+    <textarea rows={24} cols={50} onChange={(e)=>setMarkedText(e.target.value)} className='textarea'></textarea>
   )
 }
 
